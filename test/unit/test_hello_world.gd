87d99850a3835fe276d15ea386157859a21d1814
Jason Hatton <jason@hatton.io>
2020-07-24 00:49:09 -0500

    now that fails work, make passing

--- test/unit/test_hello_world.gd
+++ test/unit/test_hello_world.gd
@@ -13,7 +13,7 @@ func after_all():
 	gut.p("ran run teardown", 2)
 
 func test_assert_eq_number_equal():
-    assert_eq(1, 2, "1 == 1")
+    assert_eq(1, 1, "1 == 1")
 
 func test_hello_world():
     assert_true(true, "Hello world from unit tests!")
