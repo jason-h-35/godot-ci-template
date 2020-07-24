4f9b3bad456cc6022442aa948cd46d53e306d56c
Jason Hatton <jason@hatton.io>
2020-07-24 02:06:27 -0500

    add LANG

--- gut.sh
+++ gut.sh
@@ -11,6 +11,7 @@ echo "░ ░   ░  ░░░ ░ ░   ░           ░         ░   ░  �
 echo "      ░    ░                             ░  ░      ░                 ░  ";
 echo "                                                                        ";
 
+LANG="en_US.utf8"
 GODOT_VERSION=3.2.2
 GODOT_BIN=Godot_v${GODOT_VERSION}-stable_linux_headless.64
 GODOT_ZIP=${GODOT_BIN}.zip
@@ -21,6 +22,6 @@ if [[ -z "${GITHUB_WORKSPACE}" ]]; then
 fi
 
 curl -s -S ${GODOT_URL} --output ${GODOT_ZIP}
-unzip ${GODOT_ZIP}
+unzip -q ${GODOT_ZIP}
 chmod +x ${GODOT_BIN}
 ./${GODOT_BIN} -d -s --path $GITHUB_WORKSPACE addons/gut/gut_cmdln.gd -gdir=res://test -ginclude_subdirs -gexit
